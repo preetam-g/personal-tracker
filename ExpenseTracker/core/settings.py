4c1ae5f659753807a82575b3bd6302481ad3fa98
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2026-03-23 22:07:35 +0530

    updated settings.py

--- ExpenseTracker/core/settings.py
+++ ExpenseTracker/core/settings.py
@@ -107,9 +107,12 @@ if not DEBUG:
 
 # 10. EMAIL SETUP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 30 # seconds
+
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = f"Expense Tracker <{EMAIL_HOST_USER}>"
