0552a5946751e1e24c9a72f03fb3eef78933d054
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2026-04-01 15:13:51 +0530

    updated expense model

--- ExpenseTracker/apps/expenses/models.py
+++ ExpenseTracker/apps/expenses/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 from .managers import ExpenseManager
 from apps.base.models import SoftDeleteModel
 
@@ -30,9 +31,9 @@ class Expense(SoftDeleteModel):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='expenses')
 
-    date = models.DateTimeField()
+    date = models.DateTimeField(null=False, blank=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
-    note = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=50)
 
     category = models.ForeignKey(ExpenseCategory, on_delete=models.SET_NULL, null=True, blank=True)
     type = models.ForeignKey(ExpenseType, on_delete=models.SET_NULL, null=True, blank=True)
@@ -49,4 +50,4 @@ class Expense(SoftDeleteModel):
         ]
 
     def __str__(self):
-        return f"{self.amount} on {self.date.date()}"
+        return f"{self.amount} on {timezone.localdate(self.date)}"
