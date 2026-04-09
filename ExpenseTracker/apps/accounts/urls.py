4bbe441ef4a96e46089d72a2cc6f667e1817cf80
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2026-04-09 18:35:01 +0530

    structured accounts templates

--- ExpenseTracker/apps/accounts/urls.py
+++ ExpenseTracker/apps/accounts/urls.py
@@ -11,20 +11,20 @@ urlpatterns = [
     path("password-change/", views.password_change_view, name="password_change"),
     path("password-reset/",
          auth_views.PasswordResetView.as_view(
-             template_name="accounts/password_reset.html",
-             email_template_name="accounts/password_reset_email.html",
+             template_name="accounts/password_reset/password_reset.html",
+             email_template_name="accounts/password_reset/password_reset_email.html",
              success_url=reverse_lazy("accounts:password_reset_done")
          ), name="password_reset"),
     path("password-reset/done/",
          auth_views.PasswordResetDoneView.as_view(
-            template_name="accounts/password_reset_done.html",
+            template_name="accounts/password_reset/password_reset_done.html",
          ), name="password_reset_done"),
     path("password-reset/confirm/<uidb64>/<token>/",
          auth_views.PasswordResetConfirmView.as_view(
-             template_name="accounts/password_reset_confirm.html",
+             template_name="accounts/password_reset/password_reset_confirm.html",
              success_url=reverse_lazy("accounts:password_reset_complete"),
          ), name="password_reset_confirm"),
     path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(
-        template_name="accounts/password_reset_complete.html",
+        template_name="accounts/password_reset/password_reset_complete.html",
     ), name="password_reset_complete"),
 ]
